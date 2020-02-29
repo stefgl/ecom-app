cd890fe694ea0ab0005d9552f45241b73d4d261f
Stefan Glaum <stefan@glaum.de>
2020-02-29 09:18:54 +0100

    add preview-component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div><h1>Hats Page</h1></div>
-)
 function App() {
   return (
-    <div className="App">
+    <div>
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
