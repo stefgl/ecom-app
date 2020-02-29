2ee357725e1b0db201da226e2f7fa209a0f00e90
Stefan Glaum <stefan@glaum.de>
2020-02-29 07:18:44 +0100

    Menu with images and transitions

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,46 +1,13 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
-export const Homepage = () => (
+const Homepage = () => (
     <div className='homepage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>Hats</h1>
-                  <span className='subtitle'>Shop now</span>
-              </div>
-            </div>
-            <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>Jackets</h1>
-                  <span className='subtitle'>Shop now</span>
-              </div>
-            </div>
-            <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>Sneakers</h1>
-                  <span className='subtitle'>Shop now</span>
-              </div>
-            </div>
-            <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>Diverse</h1>
-                  <span className='subtitle'>Shop now</span>
-              </div>
-            </div>
-            <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>Women</h1>
-                  <span className='subtitle'>Shop now</span>
-              </div>
-            </div>
-            <div className='menu-item'>
-              <div className='content'>
-                  <h1 className='title'>Men</h1>
-                  <span className='subtitle'>Shop now</span>
-              </div>
-            </div>
-        </div>
+       <Directory />
     </div>
 )
+
+export default Homepage;
