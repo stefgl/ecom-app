6645e10a609d09d6552abb70670dbeace4f0730a
Stefan Glaum <stefan@glaum.de>
2020-03-01 17:59:45 +0100

    Cart can be hidden

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import './header.styles.scss';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser,hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'><Logo className='logo'/></Link>
         <div className='options'>
@@ -23,10 +23,11 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
-        <CartDropdown />
+        { hidden ? null : 
+        <CartDropdown />}
     </div>
 )
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,hidden
 })
 export default connect(mapStateToProps)(Header);
