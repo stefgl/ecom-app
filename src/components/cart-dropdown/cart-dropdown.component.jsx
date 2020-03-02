8b546b371e4ab5d7c83e03ddb06c5c0980e170e7
Stefan Glaum <stefan@glaum.de>
2020-03-02 08:13:23 +0100

    cart-items elemet added tor cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import  CustomButton from '../custom-button/cumstom-button.component'
+import CustomButton from "../custom-button/cumstom-button.component";
+import CartItem from "../cart-item/cart-item.component";
+import "./cart-dropdown.styles.scss";
 
-import './cart-dropdown.styles.scss'
-
-const CartDropdown = () => (
-    <div className='cart-dropdown'>
-        <div className='cart-items' />
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+const CartDropdown = ({items}) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+    {
+        items.map(item => 
+            <CartItem key={item.id} item={item} />
+        )
+    }
     </div>
-)
 
-export default CartDropdown;
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+const mapStateToProps = ({cart: {items}}) => ({
+    items
+})
+export default connect(mapStateToProps)(CartDropdown);
